394b71a0b2864c34320ef4e3622457045cb654a2
Rohith Srivathsav <rohithsrivathsav@gmail.com>
2021-06-01 03:08:41 +0530

    invalid safe address case handled

--- src/store/global/saga.js
+++ src/store/global/saga.js
@@ -1,10 +1,15 @@
 import { call, put, fork, takeLatest } from "redux-saga/effects";
+import { isAddress } from "@ethersproject/address";
+
 import { GET_SAFE_INFO } from "./action-types";
 import { getSafeInfoSuccess, getSafeInfoError, setReadOnly } from "./actions";
 import request from "utils/request";
 import { getSafeInfoEndpoint } from "constants/endpoints";
+import { logoutUser } from "store/logout/actions";
 
 function* fetchSafeInfo(action) {
+  if (!isAddress(action.safeAddress)) yield put(logoutUser());
+
   const requestURL = `${getSafeInfoEndpoint}?safeAddress=${action.safeAddress}&ownerAddress=${action.ownerAddress}`;
 
   const options = {
