345ce99d02847b503b44f623af463556a8ed477d
Dima Koshevoy <dmitri.koshevoy@gmail.com>
2021-10-20 13:44:20 +0300

    use account owner name in greeting

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-import { makeSelectOwnerName } from "store/global/selectors";
+import {
+  makeSelectIsReadOnly,
+  makeSelectOrganisationType,
+  makeSelectSafeOwners,
+} from "store/global/selectors";
 
 import OverviewCard from "./OverviewCard";
 import ExpensesCard from "./ExpensesCard";
@@ -9,13 +13,34 @@ import AssetsCard from "./AssetsCard";
 import RecentTxCard from "./RecentTxCard";
 
 import { Greeting, CardsGrid } from "./styles";
+import useActiveWeb3React from "hooks/useActiveWeb3React";
+import { getDecryptedOwnerName } from "store/invitation/utils";
+import { useEncryptionKey } from "hooks";
 
 export default function Dashboard() {
-  const ownerName = useSelector(makeSelectOwnerName());
+  const [encryptionKey] = useEncryptionKey();
+  const organisationType = useSelector(makeSelectOrganisationType());
+
+  const isReadOnly = useSelector(makeSelectIsReadOnly());
+  const safeOwners = useSelector(makeSelectSafeOwners());
+  const { account } = useActiveWeb3React();
+
+  const currentOwnerName = safeOwners.find(
+    ({ owner }) => owner.toLowerCase() === account.toLowerCase()
+  )?.name;
+  const decryptedName = currentOwnerName
+    ? getDecryptedOwnerName({
+        encryptedName: currentOwnerName,
+        encryptionKey,
+        organisationType,
+      })
+    : "";
 
   return (
     <div>
-      <Greeting>Hey, {ownerName}</Greeting>
+      {!isReadOnly && decryptedName ? (
+        <Greeting>Hey, {decryptedName}</Greeting>
+      ) : null}
       <CardsGrid>
         <OverviewCard />
         <ExpensesCard />
