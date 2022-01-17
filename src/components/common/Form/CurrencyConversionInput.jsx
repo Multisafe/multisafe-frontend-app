9b095d1864d71f66cf1e14f076c7fae056457f3d
Rohith Srivathsav <rohithsrivathsav@gmail.com>
2022-01-18 02:54:30 +0530

    allow erc20 transfers with no usd conversion

--- src/components/common/Form/CurrencyConversionInput.jsx
+++ src/components/common/Form/CurrencyConversionInput.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import Big from "big.js";
 import { Controller, useWatch } from "react-hook-form";
+import ReactTooltip from "react-tooltip";
 
 import Button from "components/common/Button";
 import SwapIcon from "assets/icons/dashboard/swap-icon.svg";
-import Img from "../Img";
+import Img from "components/common/Img";
+import InfoIcon from "assets/icons/dashboard/info-icon.svg";
 import { Input, InputRow, InputLabel } from "./styles";
 
 Big.DP = 18;
@@ -19,6 +21,7 @@ const CurrencyConversionInputField = ({
 }) => {
   const [swapped, setSwapped] = useState(false);
   const [currentTokenName, setCurrentTokenName] = useState();
+  const [showNoConversionInfo, setShowNoConversionInfo] = useState(false);
 
   const watchFiatValue = useWatch({ control, name: `${baseName}.fiatValue` });
   const watchTokenValue = useWatch({ control, name: `${baseName}.tokenValue` });
@@ -36,20 +39,31 @@ const CurrencyConversionInputField = ({
 
   useEffect(() => {
     // if either one of token/fiat value is empty, recalculate and set
-    if (watchFiatValue && !watchTokenValue && conversionRate) {
-      const tokenValue =
-        watchFiatValue && conversionRate
-          ? Big(watchFiatValue).div(Big(conversionRate)).toString()
-          : "";
+    if (Number(watchFiatValue) && !watchTokenValue && Number(conversionRate)) {
+      const tokenValue = Big(watchFiatValue)
+        .div(Big(conversionRate))
+        .toString();
+
       setValue(`${baseName}.tokenValue`, tokenValue);
-    } else if (!watchFiatValue && watchTokenValue && conversionRate) {
+    } else if (!Number(watchFiatValue) && watchTokenValue && conversionRate) {
       const fiatValue =
         watchTokenValue && conversionRate
           ? Big(watchTokenValue).mul(Big(conversionRate)).toString()
           : "";
       setValue(`${baseName}.fiatValue`, fiatValue);
+    } else if (!Number(conversionRate) && Number(watchFiatValue)) {
+      // No usd conversion found, keep fiatValue empty and show a notification
+      setValue(`${baseName}.fiatValue`, "");
     }
   }, [watchFiatValue, watchTokenValue, setValue, baseName, conversionRate]);
+
+  useEffect(() => {
+    if (!Number(conversionRate)) {
+      setShowNoConversionInfo(true);
+    } else {
+      setShowNoConversionInfo(false);
+    }
+  }, [conversionRate]);
 
   const handleToggleSwap = () => {
     const isSwapped = !swapped;
@@ -125,7 +139,7 @@ const CurrencyConversionInputField = ({
           rules={{
             required: "Fiat amount is required",
             validate: (value) => {
-              if (value <= 0) return "Please check the fiat amount";
+              if (value < 0) return "Please check the fiat amount";
 
               return true;
             },
@@ -145,7 +159,7 @@ const CurrencyConversionInputField = ({
                 onChange={(e) => {
                   const fiatValue = e.target.value;
                   const tokenValue =
-                    fiatValue && conversionRate
+                    fiatValue && Number(conversionRate)
                       ? Big(fiatValue).div(Big(conversionRate)).toString()
                       : "";
                   setValue(`${baseName}.tokenValue`, tokenValue);
@@ -156,6 +170,26 @@ const CurrencyConversionInputField = ({
             </InputRow>
           )}
         />
+
+        {showNoConversionInfo && (
+          <>
+            <Img
+              id={`no-conversion-${baseName}`}
+              src={InfoIcon}
+              alt="info"
+              data-for={`no-conversion-${baseName}`}
+              data-tip={`Fiat conversion is not <br />available for ${tokenName}`}
+              className="ml-3"
+            />
+            <ReactTooltip
+              id={`no-conversion-${baseName}`}
+              place={"top"}
+              type={"dark"}
+              effect={"solid"}
+              multiline={true}
+            />
+          </>
+        )}
       </div>
     </div>
   );
